e9151f5651c352ee28e3c67079df7eeb19f2fab9
Abra19 <evabrarova@gmail.com>
2024-01-06 15:47:16 +0300

    add 500 error handler

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -139,5 +139,12 @@ def post_checks(id):
 
 
 @app.errorhandler(404)
-def page_not_found():
+def page_not_found(err):
+    print(err)
     return render_template('error/404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(err):
+    print(err)
+    return render_template('error/500.html'), 500
